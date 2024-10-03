dd3cf1894eb4e5eba1a136848f372535b7e43673
AxelCotonGutierrez <learningmathematicas@gmail.com>
2024-10-03 10:51:51 +0200

    contador

--- contar123.js
+++ contar123.js
@@ -21,136 +21,136 @@ function playAudio(audioElement) {
 // Evento clic para el icono del megáfono para reproducir la pregunta en audio
 megaphoneIcon.addEventListener('click', () => playAudio(preguntaAudio));
 
+// Generar imágenes aleatorias
+const images = [
+  "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_1.svg.png",
+  "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_2.svg.png",
+  "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_3.svg.png"
+];
 
+let score = 0; // Contador de respuestas correctas
+let questionsCount = 0; // Contador de preguntas realizadas
+let previousImageIndex = -1; // Índice de la imagen de la pregunta anterior
 
+// Función para restablecer los resultados y mensajes del juego anterior
+function resetGame() {
+  const scoreElement = document.querySelector("#score");
+  const resultElement = document.querySelector("#result");
 
+  scoreElement.textContent = "";
+  resultElement.textContent = "";
+  scoreElement.style.color = "initial";
+  resultElement.style.color = "initial";
+}
 
-  // Generar imágenes aleatorias
-  const images = [
-    "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_1.svg.png",
-    "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_2.svg.png",
-    "https://raw.githubusercontent.com/AxelCotonGutierrez/Contarhasta3dados/master/img/D6_3.svg.png"
-  ];
+function generateQuestion() {
+  resetGame(); // Restablecer resultados y mensajes del juego anterior
 
-   let score = 0; // Contador de respuestas correctas
-  let questionsCount = 0; // Contador de preguntas realizadas
-  let previousImageIndex = -1; // Índice de la imagen de la pregunta anterior
-
-  // Función para restablecer los resultados y mensajes del juego anterior
-  function resetGame() {
+  // Verificar si se han realizado las 5 preguntas
+  if (questionsCount >= 5) {
     const scoreElement = document.querySelector("#score");
-    const resultElement = document.querySelector("#result");
+    scoreElement.textContent = `\u00A1Fin!`;
 
-    scoreElement.textContent = "";
-    resultElement.textContent = "";
-    scoreElement.style.color = "initial";
-    resultElement.style.color = "initial";
-  }
-
-  function generateQuestion() {
-    resetGame(); // Restablecer resultados y mensajes del juego anterior
-
-    // Verificar si se han realizado las 5 preguntas
-    if (questionsCount >= 5) {
-      const scoreElement = document.querySelector("#score");
-      scoreElement.textContent = `\u00A1Fin!`;
-
-      if (score === 5) {
-        scoreElement.textContent += ` \u00A1Felicidades, lo has conseguido!`;
-        scoreElement.style.color = "green";
-        playAudio(felicidadesAudio);
-      } else {
-        scoreElement.textContent += ` \u00A1Vuelve a intentarlo!`;
-        scoreElement.style.color = "red";
-        playAudio(intentarAudio);
-      }
-
-      // Mostrar botón "Volver a jugar"
-      const playAgainButton = document.querySelector("#play-again-button");
-      playAgainButton.style.display = "block";
-
-      return;
-    }
-
-    let chosenImageIndex = generateRandomImageIndex();
-
-    // Asegurarse de que la imagen no se repita consecutivamente
-    while (chosenImageIndex === previousImageIndex) {
-      chosenImageIndex = generateRandomImageIndex();
-    }
-
-    previousImageIndex = chosenImageIndex;
-
-    // Mostrar la imagen elegida en el HTML
-    const imageContainer = document.querySelector("#image-container");
-    imageContainer.innerHTML = ""; // Limpiar el contenedor de imágenes
-
-    const chosenImage = document.createElement("img");
-    chosenImage.src = images[chosenImageIndex];
-    chosenImage.alt = "Imagen";
-    chosenImage.classList.add("game-image"); // Agregar la clase "game-image" a la imagen
-    imageContainer.appendChild(chosenImage);
-
-    // Pedir al jugador que adivine
-    const guessButtons = document.querySelectorAll(".guess-button");
-
-    // Eliminar eventos click anteriores
-    guessButtons.forEach((button) => {
-      button.removeEventListener("click", handleGuess);
-    });
-
-    // Asignar eventos click nuevos
-    guessButtons.forEach((button) => {
-      button.addEventListener("click", handleGuess);
-    });
-  }
-
-  function generateRandomImageIndex() {
-    return Math.floor(Math.random() * images.length);
-  }
-
-  function handleGuess(event) {
-    const guess = parseInt(event.target.textContent);
-    const resultElement = document.querySelector("#result");
-    questionsCount++;
-
-    if (guess === previousImageIndex + 1) {
-      score++;
-      resultElement.textContent = "\u00A1Correcto!";
-      resultElement.style.color = "green";
-      playAudio(correctoAudio);
+    if (score === 5) {
+      scoreElement.textContent += ` \u00A1Felicidades, lo has conseguido!`;
+      scoreElement.style.color = "green";
+      playAudio(felicidadesAudio);
     } else {
-      resultElement.textContent = `Incorrecto, el valor de la imagen era ${previousImageIndex + 1}.`;
-      resultElement.style.color = "red";
-      playAudio(incorrectoAudio);
+      scoreElement.textContent += ` \u00A1Vuelve a intentarlo!`;
+      scoreElement.style.color = "red";
+      playAudio(intentarAudio);
     }
 
-    // Actualizar puntaje y generar la siguiente pregunta
-    const scoreElement = document.querySelector("#score");
-    scoreElement.textContent = ` ${score} respuestas correctas de ${questionsCount}`;
+    // Incrementar el contador en Firebase usando el script externo
+    incrementarContadorFirebase("Infantil/Matemáticas/Contar/3", "3dados");
 
-    setTimeout(generateQuestion, 1000);
-  }
-
-  function restartGame() {
-    // Restablecer variables de juego
-    score = 0;
-    questionsCount = 0;
-    previousImageIndex = -1;
-
-    // Ocultar botón "Volver a jugar"
+    // Mostrar botón "Volver a jugar"
     const playAgainButton = document.querySelector("#play-again-button");
-    playAgainButton.style.display = "none";
+    playAgainButton.style.display = "block";
 
-    // Reiniciar el juego
-    generateQuestion();
+    return;
   }
 
-  // Llamar a la función para iniciar el juego
-  generateQuestion();
+  let chosenImageIndex = generateRandomImageIndex();
 
-  // Agregar evento clic al botón "Volver a jugar"
+  // Asegurarse de que la imagen no se repita consecutivamente
+  while (chosenImageIndex === previousImageIndex) {
+    chosenImageIndex = generateRandomImageIndex();
+  }
+
+  previousImageIndex = chosenImageIndex;
+
+  // Mostrar la imagen elegida en el HTML
+  const imageContainer = document.querySelector("#image-container");
+  imageContainer.innerHTML = ""; // Limpiar el contenedor de imágenes
+
+  const chosenImage = document.createElement("img");
+  chosenImage.src = images[chosenImageIndex];
+  chosenImage.alt = "Imagen";
+  chosenImage.classList.add("game-image"); // Agregar la clase "game-image" a la imagen
+  imageContainer.appendChild(chosenImage);
+
+  // Pedir al jugador que adivine
+  const guessButtons = document.querySelectorAll(".guess-button");
+
+  // Eliminar eventos click anteriores
+  guessButtons.forEach((button) => {
+    button.removeEventListener("click", handleGuess);
+  });
+
+  // Asignar eventos click nuevos
+  guessButtons.forEach((button) => {
+    button.addEventListener("click", handleGuess);
+  });
+}
+
+function generateRandomImageIndex() {
+  return Math.floor(Math.random() * images.length);
+}
+
+function handleGuess(event) {
+  const guess = parseInt(event.target.textContent);
+  const resultElement = document.querySelector("#result");
+  questionsCount++;
+
+  if (guess === previousImageIndex + 1) {
+    score++;
+    resultElement.textContent = "\u00A1Correcto!";
+    resultElement.style.color = "green";
+    playAudio(correctoAudio);
+  } else {
+    resultElement.textContent = `Incorrecto, el valor de la imagen era ${previousImageIndex + 1}.`;
+    resultElement.style.color = "red";
+    playAudio(incorrectoAudio);
+  }
+
+  // Actualizar puntaje y generar la siguiente pregunta
+  const scoreElement = document.querySelector("#score");
+  scoreElement.textContent = ` ${score} respuestas correctas de ${questionsCount}`;
+
+  setTimeout(generateQuestion, 1000);
+}
+
+function restartGame() {
+  // Restablecer variables de juego
+  score = 0;
+  questionsCount = 0;
+  previousImageIndex = -1;
+
+  // Ocultar botón "Volver a jugar"
   const playAgainButton = document.querySelector("#play-again-button");
-  playAgainButton.addEventListener("click", restartGame);
+  playAgainButton.style.display = "none";
 
-   
+  // Reiniciar el juego
+  generateQuestion();
+}
+
+// Mostrar el contador al cargar la página usando el script externo
+mostrarContador("Infantil/Matemáticas/Contar/3", "3dados");
+
+// Llamar a la función para iniciar el juego
+generateQuestion();
+
+// Agregar evento clic al botón "Volver a jugar"
+const playAgainButton = document.querySelector("#play-again-button");
+playAgainButton.addEventListener("click", restartGame);
